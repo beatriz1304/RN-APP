755f03c7fb3f10dc6c893188a775a730adaa832d
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-10 20:26:42 -0300

    Feat: Create base text component

--- src/components/atoms/base-text/index.js
+++ src/components/atoms/base-text/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
+import { AppLoading } from 'expo'
 
 import styles from './styles'
 
-const BaseText = ({ children }) => {
-  return <Text style={styles.text}>{children}</Text>
+import { scaleFont } from '_styles/mixins'
+
+const BaseText = ({
+  style,
+  fontSize,
+  color,
+  lineHeight,
+  isBold,
+  isSemiBold,
+  children,
+  ...props
+}) => {
+  const chunkedStyles = [
+    styles.text,
+    fontSize && { fontSize: scaleFont(fontSize) },
+    lineHeight && { lineHeight: scaleFont(lineHeight) },
+    { color },
+    isBold && styles.textBold,
+    isSemiBold && styles.textSemiBold,
+    style,
+  ]
+
+  return (
+    <Text style={chunkedStyles} {...props}>
+      {children}
+    </Text>
+  )
 }
 
 export default BaseText
--- src/components/atoms/base-text/styles.js
+++ src/components/atoms/base-text/styles.js
@@ -1,8 +1,18 @@
 import { StyleSheet } from 'react-native'
 
+import { Colors } from '_styles'
+
 const styles = StyleSheet.create({
   text: {
     fontFamily: 'Montserrat_400Regular',
+    fontSize: 14,
+    color: Colors.GRAY,
+  },
+  isSemiBold: {
+    fontFamily: 'Montserrat_700Bold',
+  },
+  textBold: {
+    fontFamily: 'Montserrat_600SemiBold',
   },
 })
 
