e980a6376044cbb1044d4939553c12c63d34251c
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-10 20:27:45 -0300

    Feat: Add project colors

---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+export const WHITE = '#FFFFFF'
+export const BLACK = '0a0a0a'
+export const BLUE = '#0366d6'
+export const BLUE_2 = '#0c63c8'
+export const GRAY = '#586069'
+export const GRAY_2 = '#BCBCBC'
+export const GRAY_3 = '#eef3f3'
+export const BACKGROUND = '#eff0f4'
---
+++ src/styles/index.js
@@ -0,0 +1,4 @@
+import * as Colors from './colors'
+import * as Mixins from './mixins'
+
+export { Colors, Mixins }
