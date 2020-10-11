e3f8360d79e67c5fec765f5ce75c72b79e89e5c1
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-11 20:11:20 -0300

    Feat: Display readme on repository detail screen

--- src/screens/repository/index.js
+++ src/screens/repository/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import { gql, useQuery } from '@apollo/client'
-import { WebView } from 'react-native-webview'
+import Markdown from 'react-native-markdown-display'
 
-import { BaseText } from '_atoms'
+import { BaseText, Card } from '_atoms'
 import { RepositoryInfoItem } from '_molecules'
 import { StarIcon, IssueIcon, PullRequestIcon } from '_icons'
 import { Colors } from '_styles'
@@ -19,6 +19,7 @@ const GET_REPOSITORY_DETAIL = gql`
             text
           }
         }
+        url
         name
         issues(states: OPEN) {
           totalCount
@@ -43,28 +44,33 @@ const RepositoryDetail = ({ route }) => {
   if (error) return <Text>Error :(</Text>
   return (
     <View>
-      <BaseText fontSize={20} isBold style={styles.title}>
-        {data?.viewer?.repository?.name}
-      </BaseText>
-      <RepositoryInfoItem
-        icon={<StarIcon fill={Colors.WHITE} />}
-        title='Stars'
-        value={data?.viewer?.repository?.stargazerCount}
-        iconBackgroundColor={Colors.BLUE}
-      />
-      <RepositoryInfoItem
-        icon={<IssueIcon fill={Colors.WHITE} />}
-        title='Issues'
-        value={data?.viewer?.repository?.issues?.totalCount}
-        iconBackgroundColor={Colors.GREEN}
-      />
-      <RepositoryInfoItem
-        icon={<PullRequestIcon fill={Colors.WHITE} />}
-        title='Pull requests'
-        value={data?.viewer?.repository?.pullRequests?.totalCount}
-        iconBackgroundColor={Colors.PURPLE}
-      />
-      <WebView originWhitelist={['*']} source={{ html: data?.viewer?.repository?.object?.text }} />
+      <ScrollView>
+        <BaseText fontSize={20} isBold style={styles.title}>
+          {data?.viewer?.repository?.name}
+        </BaseText>
+        <RepositoryInfoItem
+          icon={<StarIcon fill={Colors.WHITE} />}
+          title='Stars'
+          value={data?.viewer?.repository?.stargazerCount}
+          iconBackgroundColor={Colors.BLUE}
+        />
+        <RepositoryInfoItem
+          icon={<IssueIcon fill={Colors.WHITE} />}
+          title='Issues'
+          value={data?.viewer?.repository?.issues?.totalCount}
+          iconBackgroundColor={Colors.GREEN}
+        />
+        <RepositoryInfoItem
+          icon={<PullRequestIcon fill={Colors.WHITE} />}
+          title='Pull requests'
+          value={data?.viewer?.repository?.pullRequests?.totalCount}
+          iconBackgroundColor={Colors.PURPLE}
+        />
+
+        <Card style={styles.readme}>
+          <Markdown>{data?.viewer?.repository?.object?.text}</Markdown>
+        </Card>
+      </ScrollView>
     </View>
   )
 }
--- src/screens/repository/styles.js
+++ src/screens/repository/styles.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from 'react-native'
+import { Mixins } from '_styles'
 
 const styles = StyleSheet.create({
-  title: { marginVertical: 24, paddingLeft: 16 },
+  title: { marginVertical: Mixins.scaleSize(24), paddingLeft: Mixins.scaleSize(16) },
+  readme: { marginTop: Mixins.scaleSize(20) },
 })
 
 export default styles
