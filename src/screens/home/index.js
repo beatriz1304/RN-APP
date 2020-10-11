e79507c3e1b673cda58084c441a737f6f4c8cf3b
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-11 13:16:49 -0300

    Feat: Update apollo client to use hooks

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { View, Text, ScrollView } from 'react-native'
-import { gql } from 'apollo-boost'
-import { Query } from 'react-apollo'
+import { gql, useQuery } from '@apollo/client'
 
 import { Banner, RepositoryItem, TextButton } from '_molecules'
 import BackgroundImage from '_images/background.png'
@@ -32,30 +31,31 @@ const GET_AVATAR = gql`
   }
 `
 
-const Home = ({ navigation }) => (
-  <Query query={GET_AVATAR}>
-    {({ loading, error, data }) => {
-      if (loading) return <Text>Loading...</Text>
-      if (error) return <Text>Error :(</Text>
-      return (
-        <ScrollView style={{ backgroundColor: Colors.BACKGROUND, marginBottom: 30 }}>
-          <View>
-            <Banner
-              backgroundImage={BackgroundImage}
-              avatar={data?.viewer?.avatarUrl}
-              userName={data?.viewer?.login}
+const Home = ({ navigation }) => {
+  const { loading, error, data } = useQuery(GET_AVATAR)
+  if (loading) return <Text>Loading...</Text>
+  if (error) return <Text>Error :(</Text>
+  return (
+    <ScrollView style={{ backgroundColor: Colors.BACKGROUND, marginBottom: 30 }}>
+      <View>
+        <Banner
+          backgroundImage={BackgroundImage}
+          avatar={data?.viewer?.avatarUrl}
+          userName={data?.viewer?.login}
+        />
+        <View style={styles.repoList}>
+          {data.viewer.repositories.nodes.map((item) => (
+            <RepositoryItem
+              name={item?.name}
+              key={item.id}
+              onPress={() => navigation.navigate('Repository', { name: item?.name })}
             />
-            <View style={styles.repoList}>
-              {data.viewer.repositories.nodes.map((item) => (
-                <RepositoryItem name={item?.name} key={item.id} />
-              ))}
-            </View>
-            <TextButton onPress={() => navigation.navigate('Repositories')}>VER MAIS</TextButton>
-          </View>
-        </ScrollView>
-      )
-    }}
-  </Query>
-)
+          ))}
+        </View>
+        <TextButton onPress={() => navigation.navigate('Repositories')}>VER MAIS</TextButton>
+      </View>
+    </ScrollView>
+  )
+}
 
 export default Home
