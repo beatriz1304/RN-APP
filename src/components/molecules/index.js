dc28098713d62163fb2a81c48c24a19d8ec40c2d
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-11 10:24:01 -0300

    Feat: Create language component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,4 @@
 export { default as Banner } from './banner'
 export { default as RepositoryItem } from './repository-item'
 export { default as TextButton } from './text-button'
+export { default as Language } from './language'
