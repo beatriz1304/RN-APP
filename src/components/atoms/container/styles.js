b5d9cf7355157afcb22695ee50b30926dbe2daf4
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-11 10:26:33 -0300

    Feat: Set color from variables

--- src/components/atoms/container/styles.js
+++ src/components/atoms/container/styles.js
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native'
 
 import { scaleSize } from '_styles/mixins'
+import { Colors } from '_styles'
 
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: scaleSize(26),
     paddingBottom: scaleSize(24),
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: Colors.WHITE,
   },
 })
 
