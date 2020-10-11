a6c4b1e9bcbfcba6ac9983f9285de3bbfa369d77
Beatriz da Silveira <beatriz1304@gmail.com>
2020-10-11 13:17:55 -0300

    Feat: Integrate repository detail with github graphql

--- src/screens/repository/index.js
+++ src/screens/repository/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+import { gql, useQuery } from '@apollo/client'
+import { WebView } from 'react-native-webview'
 
 import { BaseText } from '_atoms'
 import { RepositoryInfoItem } from '_molecules'
@@ -8,30 +10,63 @@ import { Colors } from '_styles'
 
 import styles from './styles'
 
-const RepositoryDetail = () => (
-  <View>
-    <BaseText fontSize={20} isBold style={styles.title}>
-      RN-APP
-    </BaseText>
-    <RepositoryInfoItem
-      icon={<StarIcon fill={Colors.WHITE} />}
-      title='Stars'
-      value='4'
-      iconBackgroundColor={Colors.BLUE}
-    />
-    <RepositoryInfoItem
-      icon={<IssueIcon fill={Colors.WHITE} />}
-      title='Issues'
-      value='4'
-      iconBackgroundColor={Colors.GREEN}
-    />
-    <RepositoryInfoItem
-      icon={<PullRequestIcon fill={Colors.WHITE} />}
-      title='Pull requests'
-      value='4'
-      iconBackgroundColor={Colors.PURPLE}
-    />
-  </View>
-)
+const GET_REPOSITORY_DETAIL = gql`
+  query($repo_name: String!) {
+    viewer {
+      repository(name: $repo_name) {
+        object(expression: "master:README.md") {
+          ... on Blob {
+            text
+          }
+        }
+        name
+        issues(states: OPEN) {
+          totalCount
+        }
+        pullRequests(states: OPEN) {
+          totalCount
+        }
+        stargazerCount
+        descriptionHTML
+      }
+    }
+  }
+`
+
+const RepositoryDetail = ({ route }) => {
+  const { name } = route.params
+  const { loading, error, data } = useQuery(GET_REPOSITORY_DETAIL, {
+    variables: { repo_name: name },
+  })
+
+  if (loading) return <Text>Loading...</Text>
+  if (error) return <Text>Error :(</Text>
+  return (
+    <View>
+      <BaseText fontSize={20} isBold style={styles.title}>
+        {data?.viewer?.repository?.name}
+      </BaseText>
+      <RepositoryInfoItem
+        icon={<StarIcon fill={Colors.WHITE} />}
+        title='Stars'
+        value={data?.viewer?.repository?.stargazerCount}
+        iconBackgroundColor={Colors.BLUE}
+      />
+      <RepositoryInfoItem
+        icon={<IssueIcon fill={Colors.WHITE} />}
+        title='Issues'
+        value={data?.viewer?.repository?.issues?.totalCount}
+        iconBackgroundColor={Colors.GREEN}
+      />
+      <RepositoryInfoItem
+        icon={<PullRequestIcon fill={Colors.WHITE} />}
+        title='Pull requests'
+        value={data?.viewer?.repository?.pullRequests?.totalCount}
+        iconBackgroundColor={Colors.PURPLE}
+      />
+      <WebView originWhitelist={['*']} source={{ html: data?.viewer?.repository?.object?.text }} />
+    </View>
+  )
+}
 
 export default RepositoryDetail
